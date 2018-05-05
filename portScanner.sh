ea722b2ae8892d8ffd0ba5f7d44bc6473178f67d
igithub14 <igithub14@users.noreply.github.com>
2018-05-05 22:56:54 +0200

    Update portScanner.sh

--- portScanner.sh
+++ portScanner.sh
@@ -6,18 +6,9 @@
 
 # If a Host is on, then the Port TCP 53 (DNS) is checked.
 
-# TO BE DONE: add other interesting Ports (80, 8080, 69, 20, 21, 22...). Try the Script.
+# TO BE DONE: add other interesting Ports (80, 8080, 69, 20, 21, 22...). 
 
-# --------------------------------------- NOTES:---------------------------------------
-# $? identifies the exit status of the last executed command 
-# "0" stands for tue
-# "1" stands for false
-# 
-# On line 28 you can see that, if the ping command has no success, 
-# the execution continues to the next iteration (then, "continue" is a sort of a jumper).
-# 
-# With regard to line 29 remember that, when executing a command on a /dev/tcp/$host/$port 
-# pseudo-device file, Bash opens a TCP connection to the associated socket.
+# Boh may work against a Linux device (because of echo > /dev/tcp...).
 
 network="192.168.1."
 port=53
@@ -27,3 +18,15 @@ for host in {1..254}; do
 	(echo > /dev/tcp/$network$host/$port) &>/dev/null
 	[ $? -eq 0 ] && [ $port -eq 53 ] && echo "$network$host DNS Server"	
 done
+
+
+# --------------------------------------- NOTES:---------------------------------------
+# $? identifies the exit status of the last executed command 
+# "0" stands for tue
+# "1" stands for false
+# 
+# On line 15 you can see that, if the ping command has no success, 
+# the execution continues to the next iteration (then, "continue" is a sort of a jumper).
+# 
+# With regard to line 16 remember that, when executing a command on a /dev/tcp/$host/$port 
+# pseudo-device file, Bash opens a TCP connection to the associated socket.
