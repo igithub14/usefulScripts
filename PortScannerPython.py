84617835f1e405276c539989bfc15ee04fec18f1
igithub14 <igithub14@users.noreply.github.com>
2018-05-06 02:05:16 +0200

    Create PortScannerPython.py

---
+++ PortScannerPython.py
@@ -0,0 +1,9 @@
+#!/usr/bin/pyhton
+
+//Remember to chmod +x the script before trying to execute it.
+
+import socket
+
+ip = raw_input("Enter the IP address: ") //ask for the target IP
+port = input("Enter the Port number: ") //ask for the taget Port
+
