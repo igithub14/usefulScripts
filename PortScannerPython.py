3529f5856044c7c9ec97723bc8f9e7cfe12c2b6f
igithub14 <igithub14@users.noreply.github.com>
2018-05-06 02:14:18 +0200

    Update PortScannerPython.py

--- PortScannerPython.py
+++ PortScannerPython.py
@@ -1,9 +1,15 @@
 #!/usr/bin/pyhton
 
-//Remember to chmod +x the script before trying to execute it.
+//Remember to chmod the script before trying to execute it.
 
 import socket
 
 ip = raw_input("Enter the IP address: ") //ask for the target IP
 port = input("Enter the Port number: ") //ask for the taget Port
 
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+if sock.connect_ex((ip.port)): //if I get a true value
+  print "Port", port, "is closed"
+else:
+  print "Port", port, "is open"
