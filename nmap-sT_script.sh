26c15b7503571a9f4798c9b03ffb77b0c74916ed
igithub14 <igithub14@users.noreply.github.com>
2018-05-09 21:55:40 +0200

    Update nmap-sT_script.sh

--- nmap-sT_script.sh
+++ nmap-sT_script.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+echo "Enter the first IP Address :"
+read firstIP
+
+echo "Enter the last IP Address :"
+read lastIP
+
+echo "Enter the Port Number you're interested in :"
+read port
+
+nmap -sT $firstIP-$lastIP -p $port -oG network
+
+cat network | grep open > networkopen
+
+cat networkopen | cut -f2 -d ":" | cut -f1 -d "(" > networkvuln
+
+cat networkvuln
